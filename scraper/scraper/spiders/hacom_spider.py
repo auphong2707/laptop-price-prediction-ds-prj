0bb5455b2d83045c691d01a233c8a9dd5293baeb
KleepyHunter <gargaggr@gmail.com>
2024-10-10 09:45:51 +0700

    Update hacom_spider.py

--- scraper/scraper/spiders/hacom_spider.py
+++ scraper/scraper/spiders/hacom_spider.py
@@ -9,7 +9,7 @@ class HacomSpider(BaseLaptopshopLoadmoreButtonSpider):
     start_urls = [
         "https://hacom.vn/laptop"
     ]
-    product_site_css = "h3.p-name a::attr(href)"
+    product_site_css = ".p-name a::attr(href)"
     loadmore_button_css = ".btn-view-more"
 
     def parse_brand(self, response: Response):
@@ -36,8 +36,46 @@ class HacomSpider(BaseLaptopshopLoadmoreButtonSpider):
         try:
             cpus = response.css('div.pd-summary-group div div.item::text').getall()
             for cpu in cpus:
-                if "CPU" in cpu or "xử lý" in cpu:
-                    return cpu.split('CPU: ')[1].split('(')[0].split('\r')[0]
+                if "CPU" in cpu:
+                    return cpu.split('CPU')[1].split('(')[0].split('\r')[0].strip(' :')
+                elif "xử lý" in cpu:
+                    return cpu.split('xử lý')[1].split('(')[0].split('\r')[0].strip(' :')
         except:
             return "N/A"
     
+    def parse_vga(self, response: Response):
+        try:
+            vgas = response.css('div.pd-summary-group div div.item::text').getall()
+            for vga in vgas:
+                if "VGA" in vga:
+                    return vga.split('VGA')[-1].split('(')[0].split('\r')[0].strip(' :()')
+                elif "đồ họa" in vga:
+                    return vga.split('đồ họa')[-1].split('(')[0].split('\r')[0].strip(' :()')
+        except:
+            return "N/A"
+        
+    def parse_ram_amount(self, response: Response):
+        try:
+            rams = response.css('div.pd-summary-group div div.item::text').getall()
+            for ram in rams:
+                if "RAM" in ram:
+                    if "onboard" in ram:
+                        tmp = ram.split('onboard')[0].split('Onboard')[0].strip(' :()').split()
+                        tp = ''
+                        for tm in tmp:
+                            if "GB" in tm:
+                                if tp.isnumeric():
+                                    return tp+tm
+                                return tm
+                            tp = tm
+                    else:
+                        tmp = ram.split()
+                        tp = ''
+                        for tm in tmp:
+                            if "GB" in tm:
+                                if tp.isnumeric():
+                                    return tp+tm
+                                return tm
+                            tp = tm
+        except:
+            return "N/A"
