41ee286396f9b1a5af75988cf4785c8d8072ae64
cssi87m <daoxuanquangminh2004@gmail.com>
2024-10-23 10:00:22 +0700

    small update

--- scraper/scraper/spiders/nguyenkim_spider.py
+++ scraper/scraper/spiders/nguyenkim_spider.py
@@ -11,67 +11,13 @@ class NguyenkimScraper(BaseLaptopshopPageSpider):
     start_urls = ['https://www.nguyenkim.com/laptop-may-tinh-xach-tay']
     allowed_domains = ['nguyenkim.com']
     product_site_css = "h2.product-title a::attr(href)"
-    page_css = "a.ty-pagination__item::attr(href)"
-    COOKIES_ENABLED = True
-    COOKIES_DEBUG = True
-
-    def start_requests(self):
-        # Use a regular Request, handle redirects manually
-        yield Request(
-            url='https://www.nguyenkim.com/laptop-may-tinh-xach-tay',
-            callback=self.parse,
-            meta={'handle_httpstatus_list': [302]}  # Key change: handle 302 redirects
-        )
-
-    def parse(self, response: Response):
-        # Check for redirect as before
-        if response.status in (301, 302, 307):
-            logging.info(f"Following redirect to: {response.headers['Location']}")
-            yield Request(response.urljoin(response.headers['Location'].decode()), callback=self.parse)  # Call parse again after redirect
-
-        else:  # Proceed with normal parsing if not a redirect
-            # Use the base class's get_product_sites method
-            for site_request in self.get_product_sites(response):
-                yield site_request
-
-            # Pagination (using the base class's logic)
-            for page in response.css(self.page_css).getall():
-                yield response.follow(url=page, callback=self.parse)
-    
-    def parse_one_observation(self, response: Response):
-        # Create an empty item dictionary
-        item = {}
-
-        # Initiate the SeleniumRequest, passing the item through meta
-        yield SeleniumRequest(
-            url=response.url,
-            callback=self.parse_product_selenium,  # New callback function
-            wait_time=3,
-            screenshot=True,
-            meta={'item': item}
-        )
-    
-    def parse_product_selenium(self, response: Response):
-        # Retrieve the item from meta
-        item = response.meta['item']
-
-        # Now perform all your parsing logic using Selenium
-        item['brand'] = self.parse_brand(response)
-        item['name'] = self.parse_name(response)
-        item['cpu'] = self.parse_cpu(response)
-        
-        item['price'] = self.parse_price(response)
-
-        # Yield the populated item
-        yield item
-
+    page_css = "a.page.cm-history.ty-pagination__item::attr(href)"
+    show_technical_spec_button_xpath = '//*[@id="productSpecification_viewFull"]'
+    source = "nguyenkim"
     
     def get_scoped_value(self, response, names):
         possibile_values = [
-                "//tr/td[contains(., '{}')]/following-sibling::td//span//text()".format(name)
-                for name in names
-            ] + [
-                "//li/div[contains(., '{}')]/following-sibling::div/text()".format(name)
+                "//tr/td[contains(., '{}')]/following-sibling::td/text()".format(name)
                 for name in names
             ]
         for value in possibile_values:
