a996d0a592feb5bbab39f5792c12e19b52fcf949
Veil <65228307+auphong2707@users.noreply.github.com>
2024-10-24 17:37:28 +0700

    Update nguyenkim_spider.py

--- scraper/scraper/spiders/nguyenkim_spider.py
+++ scraper/scraper/spiders/nguyenkim_spider.py
@@ -1,19 +1,16 @@
-import scrapy
-from scrapy.http import Response, Request
-import re
-import logging 
+from scrapy.http import Response
 from .base_laptopshop_spider import BaseLaptopshopPageSpider
-from scrapy_selenium import SeleniumRequest
 
 # create scraper
 class NguyenkimScraper(BaseLaptopshopPageSpider):
     name = "nguyenkim_spider"
-    start_urls = ['https://www.nguyenkim.com/laptop-may-tinh-xach-tay']
-    allowed_domains = ['nguyenkim.com']
+    start_urls = ['https://www.nguyenkim.com/laptop-may-tinh-xach-tay/']
     product_site_css = "h2.product-title a::attr(href)"
     page_css = "a.page.cm-history.ty-pagination__item::attr(href)"
     show_technical_spec_button_xpath = '//*[@id="productSpecification_viewFull"]'
     source = "nguyenkim"
+    selenium_page_request = True
+    selenium_product_request = True
     
     def get_scoped_value(self, response, names):
         possibile_values = [
