a89cfb1146d9ba66997cfcfd734a91d2135a1f9c
TheSyx <minhquantsy@gmail.com>
2024-10-25 01:28:08 +0700

    Update laptop88_spider.py

--- scraper/scraper/spiders/laptop88_spider.py
+++ scraper/scraper/spiders/laptop88_spider.py
@@ -7,7 +7,9 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
     allowed_domains = ["laptop88.vn"]
     start_urls = [
         "https://laptop88.vn/laptop-moi.html",
-    ]
+        ] + [
+        f"https://laptop88.vn/laptop-moi.html?page={i}" for i in range(2, 15)
+        ]
     
     product_site_css = 'h2.product-title a::attr(href)'
     source = 'laptop88'
@@ -17,7 +19,25 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         possible_values = [
             "//td[strong[text()='{}']]/following-sibling::td/text()".format(name)
             for name in names
+        ] + [
+            "//tr[td//strong[contains(text(), '{}')]]//text()".format(name)
+            for name in names
+        ] + [
+            "//tr[td[contains(text(),'{}')]]/td[@class='alignleft']/div[@class='rightValue']/text()".format(name)
+            for name in names
+        ] + [
+            "//tr[td[1][contains(text(), '{}')]]/td[2]//span[@class='configuration-content fl']".format(name)
+            for name in names
+        ] + [
+            "//tr[th[contains(text(), '{}')]]/td//text()".format(name)
+            for name in names
+        ] + [
+            "//tr[td[1][contains(., '{}')]]/td[2]//text()".format(name)
+            for name in names
+        ] + [
+            
         ]
+        
         for value in possible_values:
             scope = response.xpath(value).getall()
             if len(scope) > 0:
@@ -29,6 +49,14 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         """
         Returns True if the response is valid to be scraped.
         """
+        product_name = response.xpath("//h2[@class='name-product']/text()").get().lower()
+        for _ in ["ipad", "tablet", "cũ"]:
+            if _ in product_name:
+                return False
+        
+        price = response.xpath("//div[@class='price js-price-config js-price-buildpc']/text()").get().lower()
+        if "liên hệ" in price or "call" in price:
+            return False
         return True
     
     # [PARSE FEATURES SECTION: START]
@@ -39,12 +67,15 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         Example: Dell, HP, etc.
         """
         try:
-            product_name = response.xpath("//h2[@class='name-product']/text()").getall().lower()
+            product_name = response.xpath("//h2[@class='name-product']/text()").get().lower()
             for brand in ["dell", "asus", "lenovo", "hp", "msi", "acer", "huawei", "gigabyte", "samsung galaxy", "lg", "microsoft"]:
                 if brand in product_name:
                     return brand
-            if "macbook" in brand:
+            if "macbook" in product_name:
                 return "apple"
+            for name in ["thinkpad", "ideapad"]:
+                if name in product_name:
+                    return "lenovo"
         except:
             return "n/a"
     
@@ -53,16 +84,17 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         Extracts the name of the laptop from the response.
         """
         try:
-            product_name = response.xpath("//h2[@class='name-product']/text()").getall()
+            product_name = response.xpath("//h2[@class='name-product']/text()").get().lower()
+            return product_name if product_name else "n/a"
         except:
-            return "N/A"
+            return "n/a"
     
     # CPU
     def parse_cpu(self, response: Response):
         """
         Extracts the CPU name of the laptop from the response.
         """
-        cpu = self.get_scoped_values(response, ['CPU', 'Processor', 'Tên bộ vi xử lý'])
+        cpu = self.get_scoped_values(response, ['CPU', 'Processor', 'Tên bộ vi xử lý', 'CPU:'])
         return cpu if cpu else 'n/a'
     
     # VGA
@@ -78,7 +110,7 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         """
         Extracts the amount of RAM in GB from the response.
         """
-        ram_amount = self.get_scoped_values(response, ['Memomy', 'Dung lượng', 'RAM', 'Ram:'])
+        ram_amount = self.get_scoped_values(response, ['Memory', 'Dung lượng', 'RAM', 'Ram:', 'Bộ nhớ trong - Ram', "Bộ nhớ", ])
         return ram_amount if ram_amount else 'n/a'
     
     def parse_ram_type(self, response: Response): 
@@ -86,21 +118,24 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         Extracts the type of RAM from the response.
         Example: DDR3, DDR4, etc.
         """
-        return "N/A"
+        ram = self.get_scoped_values(response, ['Memory', 'Dung lượng', 'RAM', 'Ram:'])
+        return ram if ram else 'n/a'
     
     # Storage
     def parse_storage_amount(self, response: Response): 
         """
         Extracts the amount of storage in GB from the response.
         """
-        return "N/A"
+        storage = self.get_scoped_values(response, ['Ổ cứng', 'Storage', 'Dung lượng ổ cứng', 'Ổ cứng:'])
+        return storage if storage else 'n/a'
     
     def parse_storage_type(self, response: Response): 
         """
         Extracts the type of storage from the response.
         Example: HDD, SSD, SSHD.
         """
-        return "N/A"
+        storage = self.get_scoped_values(response, ['Ổ cứng', 'Storage', 'Dung lượng ổ cứng', 'Ổ cứng:'])
+        return storage if storage else 'n/a'
     
     # Webcam
     def parse_webcam_resolution(self, response: Response): 
@@ -108,52 +143,60 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         Extracts the webcam resolution from the response.
         Example: HD, FHD, 4K.
         """
-        return "N/A"
+        webcam = self.get_scoped_values(response, ['Webcam', 'Camera', 'Webcam:'])
+        return webcam if webcam else 'n/a'
     
     # Screen
     def parse_screen_size(self, response: Response): 
         """
         Extracts the screen size in inches from the response.
         """
-        return "N/A"
+        screen = self.get_scoped_values(response, ['Display', 'Độ phân giải:', 'Màn hình'])
+        return screen if screen else 'n/a'
     
     def parse_screen_resolution(self, response: Response): 
         """
         Extracts the screen resolution from the response.
         Example: HD, FHD, 4K.
         """
-        return "N/A"
+        screen = self.get_scoped_values(response, ['Display', 'Độ phân giải:', 'Màn hình'])
+        return screen if screen else 'n/a'
 
     def parse_screen_refresh_rate(self, response: Response): 
         """
         Extracts the screen refresh rate in Hz from the response.
         """
-        return "N/A"
+        screen = self.get_scoped_values(response, ['Display', 'Độ phân giải:', 'Màn hình'])
+        return screen if screen else 'n/a'
     
     def parse_screen_brightness(self, response: Response): 
         """
         Extracts the screen brightness in nits from the response.
         """
-        return "N/A"
+        screen = self.get_scoped_values(response, ['Display', 'Độ phân giải:', 'Màn hình'])
+        return screen if screen else 'n/a'
     
     # Battery
     def parse_battery_capacity(self, response: Response): 
         """
         Extracts the battery capacity in Whr from the response.
         """
-        return "N/A"
+        battery = self.get_scoped_values(response, ['Pin', 'Battery'])
+        return battery if battery else "n/a"
     
     def parse_battery_cells(self, response: Response):
         """
         Extracts the number of battery cells from the response.
         """
-        return "N/A"
+        battery = self.get_scoped_values(response, ['Pin', 'Battery'])
+        return battery if battery else "n/a"
     
     # Size
     def parse_size(self, response: Response):
         """
         Extracts the size of the laptop in cm from the response.
         """
+        size = self.get_scoped_values(response, ['Kích thước', 'Dimensions'])
         return "N/A"
     
     # Weight
@@ -161,29 +204,33 @@ class Laptop88Spider(BaseLaptopshopPageSpider):
         """
         Extracts the weight of the laptop in kg from the response.
         """
-        return "N/A"
-    
+        weight = self.get_scoped_values(response, ['Trọng lượng', 'Weight'])
+        return weight if weight else "n/a"
+
     # Connectivity
     def parse_connectivity(self, response: Response):
         """
         Extracts the connectivity options of the laptop from the response.
         """
-        return "N/A"
-    
+        res = self.get_scoped_values(response, ['Cổng kết nối'])
+        return res if res else "n/a"
+        
     # Operating System
     def parse_default_os(self, response: Response): 
         """
         Extracts the default operating system of the laptop from the response.
         Example: Windows, Linux, etc.
         """
-        return "N/A"
+        os = self.get_scoped_values(response, ['Hệ điều hành', 'Operating System'])
+        return os if os else 'n/a'
     
     # Warranty
     def parse_warranty(self, response: Response): 
         """
         Extracts the warranty period in months from the response.
         """
-        return "N/A"
+        warranty = response.xpath("//div[@class='product-warranty']//p[contains(text(), 'Bảo hành')]/text()").get()
+        return warranty if warranty else "n/a"
     
     # Price
     def parse_price(self, response: Response): 
