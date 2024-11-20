f3ca0ce1728c7e9ac12fd40e9f69c4a59d0a880f
TheSyx <minhquantsy@gmail.com>
2024-11-20 21:47:28 +0700

    Update laptopworld_spider.py

--- scraper/scraper/spiders/laptopworld_spider.py
+++ scraper/scraper/spiders/laptopworld_spider.py
@@ -34,7 +34,9 @@ class LaptopworldSpider(BaseLaptopshopPageSpider):
     
     def yield_condition(self, response):
         name = response.xpath('//h1/text()').get().lower()
-        if any(_ in name for _ in ['balo', 'tai nghe', 'dock', 'máy chơi game']):
+        price = response.xpath('//b[@class="js-this-product"]/text()').get().lower()
+        if any(_ in name for _ in ['balo', 'tai nghe', 'dock', 'máy chơi game']) \
+            or (price is not None and 'liên hệ' in price):
             print(f"Skipped: {name}")
             return False
         return True
