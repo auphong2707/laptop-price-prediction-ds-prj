1b0b72b8f2103ab018da59fc98a9b7cfa2be34a8
Veil <65228307+auphong2707@users.noreply.github.com>
2024-11-30 18:03:23 +0700

    Update Database

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+import io
+from flask import Flask, render_template, request, redirect, send_file, url_for
+import pandas as pd
+from helper import *
+
+TABLE_LIST = get_table_list()
 
 app = Flask(__name__)
 
@@ -21,14 +26,35 @@ def data_analysis():
     analysis_results = {"mean": 50, "median": 45, "std_dev": 5}  # Dummy data
     return render_template('data_analysis.html', results=analysis_results)
 
-@app.route('/database')
-def database():
-    sample_data = [
-        {"id": 1, "name": "Item A", "value": 100},
-        {"id": 2, "name": "Item B", "value": 200},
-        {"id": 3, "name": "Item C", "value": 300}
-    ]
-    return render_template('database.html', data=sample_data)
+@app.route('/database/<table_name>')
+def database(table_name):
+    if table_name == 'default_table':
+        return redirect(url_for('database', table_name=TABLE_LIST[0]))
+    
+    df = get_table(table_name)
+    
+    # Convert DataFrame to HTML
+    table_html = df.to_html(classes='dataframe', index=False, border=0)
+    
+    return render_template('database.html', 
+                           table_name=table_name, 
+                           table_list=TABLE_LIST, 
+                           table_html=table_html)
+    
+@app.route('/download_csv/<table_name>')
+def download_csv(table_name):
+    df = get_table(table_name)
+    
+    # Convert the DataFrame to a CSV in memory
+    csv = df.to_csv(index=False)
+    
+    # Create an in-memory binary stream to serve as the file
+    response = io.BytesIO()
+    response.write(csv.encode())
+    response.seek(0)
+    
+    # Send the file to the user
+    return send_file(response, mimetype='text/csv', as_attachment=True, download_name=f'{table_name}.csv')
 
 
 if __name__ == '__main__':
