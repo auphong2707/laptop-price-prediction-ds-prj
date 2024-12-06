9285f4071bbdb5952e1a3d271d74c86f2baf8c1f
Veil <65228307+auphong2707@users.noreply.github.com>
2024-12-06 23:23:21 +0700

    Add EDA to UI

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,9 +1,12 @@
 import io
-from flask import Flask, render_template, request, redirect, send_file, url_for
-import pandas as pd
+import os
+from flask import Flask, render_template, request, redirect, send_file, send_from_directory, url_for
+import pdfkit
 from helper import *
 
 TABLE_LIST = get_table_list()
+EDA_PATH = '/app/data_analysis/results/eda/'
+EDA_LIST = [f for f in os.listdir(EDA_PATH) if os.path.isfile(os.path.join(EDA_PATH, f))]
 
 app = Flask(__name__)
 
@@ -21,10 +24,55 @@ def predictor():
         return render_template('predictor.html', prediction=prediction)
     return render_template('predictor.html')
 
-@app.route('/data_analysis')
-def data_analysis():
-    analysis_results = {"mean": 50, "median": 45, "std_dev": 5}  # Dummy data
-    return render_template('data_analysis.html', results=analysis_results)
+@app.route('/data_analysis/<eda_name>')
+def data_analysis(eda_name):
+    if eda_name == 'default_eda':
+        # Redirect to the first EDA file if none is specified
+        return redirect(url_for('data_analysis', eda_name=EDA_LIST[0]))
+    
+    # Pass the list of files and the active file to the template
+    return render_template(
+        "data_analysis.html", 
+        eda_list=EDA_LIST, 
+        active_eda=eda_name
+    )
+    
+@app.route('/download_pdf/<eda_name>')
+def download_pdf(eda_name):
+    file_path = os.path.join(EDA_PATH, eda_name)
+    
+    if os.path.isfile(file_path):
+        # Define PDF options to adjust width
+        options = {
+            'page-width': '300mm',
+            'page-height': '297mm',
+            'margin-top': '10mm',
+            'margin-right': '10mm',
+            'margin-bottom': '10mm',
+            'margin-left': '10mm'
+        }
+        
+        # Convert HTML file to PDF with the above options
+        pdf_output = pdfkit.from_file(file_path, False, options=options)
+        
+        # Return the PDF as a response
+        return send_file(
+            io.BytesIO(pdf_output), 
+            mimetype='application/pdf', 
+            as_attachment=True, 
+            download_name=f"{eda_name.replace('.html', '.pdf')}"
+        )
+    else:
+        return f"File {eda_name} not found", 404
+
+@app.route('/serve_eda/<eda_name>')
+def serve_eda(eda_name):
+    # Serve the selected EDA file for iframe rendering
+    if os.path.isfile(os.path.join(EDA_PATH, eda_name)):
+        print(EDA_PATH, eda_name)
+        return send_from_directory(EDA_PATH, eda_name)
+    else:    
+        return f"File {eda_name} not found", 404
 
 @app.route('/database/<table_name>')
 def database(table_name):
